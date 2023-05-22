6f52f3c7f5974656c9e690968225cf995a3af77b
govind15496 <govindsingh15496@gmail.com>
2023-05-22 09:33:08 +0530

    remove unused code and files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Templates from "./components/Templates";
+import HowYamakWorks from "./components/HowYamakWorks";
+import Help from "./components/Help";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingHome from "./components/Pages/LandingHome";
+import Plans from "./components/Plans";
+import GetStarted from "./components/GetStarted";
+import PlansHeader from "./components/PlansHeader";
+import SignIn from "./components/Pages/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/"
+            element={[
+              <Header />,
+              <Home />,
+              <Templates />,
+              <HowYamakWorks />,
+              <Help />,
+              <FAQ />,
+              <Footer />,
+            ]}
+          />
+
+          <Route path="/features" element={[<Header />, <Footer />]} />
+          <Route
+            path="/pricing"
+            element={[
+              <Header />,
+              <LandingHome />,
+              <PlansHeader />,
+              <Plans />,
+              <FAQ />,
+              <GetStarted />,
+              <Footer />,
+            ]}
+          />
+          <Route
+            path="/signin"
+            element={[<Header />, <SignIn />, <Footer />]}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
